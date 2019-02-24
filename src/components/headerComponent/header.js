9d23e33a1661adfea08a83c6f723a701d59e45c5
T-Dimi <thomas.dimitriadis@icloud.com>
2019-02-23 22:34:44 -0800

    fixing dropdown bug

--- src/components/headerComponent/header.js
+++ src/components/headerComponent/header.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
 } from 'reactstrap';
 import logo from './logo.png';
 
@@ -34,42 +34,42 @@ class Header extends Component {
     return (
       <div>
         <Navbar className="header" light expand="md">
-                <NavbarBrand href="/"><img className="header-img" src={logo} alt="Logo" /><span className="header-company">Best Community Service</span></NavbarBrand>
+          <NavbarBrand href="/"><img className="header-img" src={logo} alt="Logo" /><span className="header-company">Best Community Service</span></NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-                <NavItem>
-                        <NavLink href="/"><span className="header-text">Home</span></NavLink>
-                </NavItem>
-                <NavItem>
-                        <NavLink href="/rooms"><span className="header-text">Reserve Room</span></NavLink>
-                </NavItem>
-                <NavItem>
-                        <NavLink href="/classes"><span className="header-text">Classes</span></NavLink>
-                </NavItem>
+              <NavItem>
+                <NavLink href="/"><span className="header-text">Home</span></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/rooms"><span className="header-text">Reserve Room</span></NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/classes"><span className="header-text">Classes</span></NavLink>
+              </NavItem>
 
-                <UncontrolledDropdown nav inNavbar>
+              <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
-                        <span className="header-text">Services</span>
+                  <span className="header-text">Services</span>
                 </DropdownToggle>
                 <DropdownMenu right>
-                    <DropdownItem>
-                        <NavLink href="/catering"><span className="dropdown-links">Catering</span></NavLink>
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>
-                        <NavLink href="/equipment"><span className="dropdown-links">Equipment Rental</span></NavLink>
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>
-                        <NavLink href="/shuttle"><span className="dropdown-links">Shuttle</span></NavLink>
-                    </DropdownItem>
-                    <DropdownItem divider />
-                    <DropdownItem>
-                        <NavLink href="/homecare"><span className="dropdown-links">Home Care</span></NavLink>
-                    </DropdownItem>
+                  <DropdownItem>
+                    <NavLink href="/catering"><span className="dropdown-links">Catering</span></NavLink>
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    <NavLink href="/equipment"><span className="dropdown-links">Equipment Rental</span></NavLink>
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    <NavLink href="/shuttle"><span className="dropdown-links">Shuttle</span></NavLink>
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    <NavLink href="/homecare"><span className="dropdown-links">Home Care</span></NavLink>
+                  </DropdownItem>
                 </DropdownMenu>
-                </UncontrolledDropdown>
+              </UncontrolledDropdown>
             </Nav>
           </Collapse>
         </Navbar>
