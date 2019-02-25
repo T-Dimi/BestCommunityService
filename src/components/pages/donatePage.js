6c79e91114a9e5d91d089df41884c15e81a92474
T-Dimi <thomas.dimitriadis@icloud.com>
2019-02-24 17:05:18 -0800

    Adding donation form to the donation page

--- src/components/pages/donatePage.js
+++ src/components/pages/donatePage.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
+import { Col, Row, Container, Button, Form } from 'reactstrap';
+import { RadioGroup, RadioButton} from 'react-radio-buttons';
+
+
+
 
 class DonatePage extends Component {
     render() {
         return (
-
             <div className="page">
                 <h1 className="page-title">
-                    Donate Page Content
+                    Best Community Donation
                 </h1>
-                <p>
-                    Lorem ipsum dolor sit amet, vel eripuit docendi albucius id, ea iusto vitae scriptorem nec. Novum accusata mei ex. Adhuc inani mediocritatem te eum, id ancillae detracto mea. No cum possim utamur imperdiet. Erant forensibus usu an, aeque quaestio per no.
-
-                    Blandit nominati consulatu est ad, eum fugit tritani debitis in. Munere repudiare constituam cu mel. Minimum voluptua ad vim, dico oratio percipit has at. Timeam accommodare ea his. Mea enim epicuri complectitur te, ad eum civibus intellegebat. Cum ad omittam abhorreant quaerendum.
-
-                    Soluta efficiendi cum ea. Cibo vocibus in sea, eum quem agam mucius eu, ne has
-                </p>
+                <Container>
+                    <Row>
+                    <Col>
+                    <p className="intro">
+                    Your charitable donation will help fund all of our services that we offer here at Best Community
+                    for the community. We appreciate any and all amounts that you are able to donate.
+                    </p>
+                    <Form className="volunteer-form" action="" method="">
+                    <RadioGroup className='radioGroup' onChange={ this.onChange } vertical>
+                    <RadioButton iconInnerSize={30} iconSize={40} pointColor='#112D32' value="orange">
+                            $1 - Donation to Best Community Service
+                        </RadioButton>
+                        <RadioButton iconInnerSize={30} iconSize={40} pointColor='#112D32' value="orange">
+                            $5 - Donation to Best Community Service
+                        </RadioButton>
+                        <RadioButton iconInnerSize={30} iconSize={40} pointColor='#112D32' value="orange">
+                            $25 - Donation to Best Community Service
+                        </RadioButton>
+                    </RadioGroup>
+                    <Button className="donate-btn">Contribute</Button>
+                    </Form>
+                    </Col>
+                    </Row>
+                </Container>
             </div>
         );
     }
